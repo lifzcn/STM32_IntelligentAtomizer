c08ddbfc3a1300a8c5bcc8469e2eaa2f2dcd3684
LiFZ <lifzcn@qq.com>
2023-04-22 16:44:26 +0800

    Update some files

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -60,14 +60,14 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint8_t i = 0;
-	uint16_t j = 500;
+  uint8_t i = 0;
+  uint16_t j = 500;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -91,97 +91,98 @@ int main(void)
   MX_TIM1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		if(HAL_GPIO_ReadPin(Key_1_GPIO_Port, Key_1_Pin) == GPIO_PIN_RESET)
-		{
-			HAL_Delay(50);
-			i += 1;
-		}
-		else if(HAL_GPIO_ReadPin(Key_2_GPIO_Port, Key_2_Pin) == GPIO_PIN_RESET)
-		{
-			HAL_Delay(50);
-			i += 1;
-		}
-		
-		if(i%2==1)
-		{
-			printf("智能雾化器已打开，欢迎您的使用！\n");
-			if(HAL_GPIO_ReadPin(Key_3_GPIO_Port, Key_3_Pin) == GPIO_PIN_RESET)
-			{
-				HAL_Delay(50);
-				j += 500;
-			}
-			else if(HAL_GPIO_ReadPin(Key_4_GPIO_Port, Key_4_Pin) == GPIO_PIN_RESET)
-			{
-				HAL_Delay(50);
-				j -= 500;
-			}
-		}
-		else if(i%2==0)
-		{
-			printf("智能雾化器已关闭，感谢您的使用！\n");
-			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
-			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-		}
-		
-		switch(j)
-		{
-			case 500:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-				printf("当前雾化器档位为：0 档！\n");
-			break;
-			case 1000:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-				printf("当前雾化器档位为：1 档！\n");
-			break;
-			case 1500:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-				printf("当前雾化器档位为：2 档！\n");
-			break;
-			case 2000:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 2000);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-				printf("当前雾化器档位为：3 档！\n");
-			break;
-			case 2500:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 2500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-				printf("当前雾化器档位为：4 档！\n");
-			break;
-			default:
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
-				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
-			break;
-		}
-		i = 0;
-		j = 500;
-		HAL_Delay(1000);
+    if (HAL_GPIO_ReadPin(Key_1_GPIO_Port, Key_1_Pin) == GPIO_PIN_RESET)
+    {
+      HAL_Delay(50);
+      i += 1;
+    }
+    else if (HAL_GPIO_ReadPin(Key_2_GPIO_Port, Key_2_Pin) == GPIO_PIN_RESET)
+    {
+      HAL_Delay(50);
+      i += 1;
+    }
+
+    if (i % 2 == 1)
+    {
+      printf("Atomizer: ON\n");
+      if (HAL_GPIO_ReadPin(Key_3_GPIO_Port, Key_3_Pin) == GPIO_PIN_RESET)
+      {
+        HAL_Delay(50);
+        j += 500;
+      }
+      else if (HAL_GPIO_ReadPin(Key_4_GPIO_Port, Key_4_Pin) == GPIO_PIN_RESET)
+      {
+        HAL_Delay(50);
+        j -= 500;
+      }
+    }
+    else if (i % 2 == 0)
+    {
+      printf("Atomizer: OFF\n");
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      j = 500;
+    }
+
+    switch (j)
+    {
+    case 500:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      printf("Gear: 0\n");
+      break;
+    case 1000:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      printf("Gear: 1\n");
+      break;
+    case 1500:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      printf("Gear: 2\n");
+      break;
+    case 2000:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 2000);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      printf("Gear: 3\n");
+      break;
+    case 2500:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 2500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      printf("Gear: 4\n");
+      break;
+    default:
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, 500);
+      __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_4, 500);
+      j = 500;
+      break;
+    }
+
+    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -190,17 +191,17 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -214,9 +215,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -233,9 +233,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -247,14 +247,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
